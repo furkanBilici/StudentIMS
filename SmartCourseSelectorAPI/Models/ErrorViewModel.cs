fc8b01f33ee7b9929ac040f96ab4ea3e06637486
furkanBilici <fbilici258@gmail.com>
2024-12-12 22:52:28 +0300

    projeye temiz başlangıç

--- SmartCourseSelectorAPI/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace SmartCourseSelectorAPI.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
