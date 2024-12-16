f8a84b42bed2d3cdfa7f285ad8a77f9a5495eb76
furkanBilici <fbilici258@gmail.com>
2024-12-17 01:23:26 +0300

    elimdeki projeyi daha temize çekmek için şablon eklendi

---
+++ SmartCourseSelectorAPI/Models/Student.cs
@@ -0,0 +1,18 @@
+﻿namespace SmartCourseSelectorWeb.Models
+{
+    public class Student
+    {
+        public int StudentID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public int? AdvisorID { get; set; }
+        public DateTime EnrollmentDate { get; set; }
+        public string Department { get; set; } 
+
+  
+        public Advisor Advisor { get; set; } 
+
+        public ICollection<StudentCourseSelection> StudentCourseSelections { get; set; } = new List<StudentCourseSelection>();
+    }
+}
--- StudentIMS/Models/LoginViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace StudentIMS.Models
-{
-    public class LoginViewModel
-    {
-        public string Username { get; set; }
-        public string Password { get; set; }
-        public string Role { get; set; }
-    }
-}
