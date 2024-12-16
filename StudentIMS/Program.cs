d373799601c8a7bdf7d3cbfc002ddc70cfc567c9
furkanBilici <fbilici258@gmail.com>
2024-12-17 00:37:54 +0300

    düzenleme ve appsettings düzenlemeleri

--- StudentIMS/Program.cs
+++ StudentIMS/Program.cs
@@ -1,22 +1,48 @@
+using Microsoft.EntityFrameworkCore;
+using StudentIMS.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddRazorPages();
+builder.Services.AddControllersWithViews();
+builder.Services.AddControllers();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error");
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
 }
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
 
 app.UseRouting();
 
 app.UseAuthorization();
 
-app.MapStaticAssets();
-app.MapRazorPages()
-   .WithStaticAssets();
+// CORS ekleyin (Postman'den eriþim için gerekli)
+app.UseCors(policy =>
+    policy.AllowAnyOrigin()
+          .AllowAnyMethod()
+          .AllowAnyHeader());
+
+// MVC Route (Ön yüzde çalýþmak için gerekli)
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Account}/{action=LoginUser}");
+
+// API route (Postman üzerinden çalýþmasý için gerekli)
+app.MapControllers();
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.Run();
