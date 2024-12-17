95ea07820c8d34672f162cb9d94812d8e6a141b9
furkanBilici <fbilici258@gmail.com>
2024-12-17 14:10:53 +0300

    logout tuşları ve ders seçim tarihini tutan tablo modeli güncellemesi

---
+++ SmartCourseSelectorAPI/Models/SelectionHistory.cs
@@ -0,0 +1,15 @@
+﻿using SmartCourseSelectorWeb.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace StudentIMS.Models
+{
+    public class SelectionHistory
+    {
+        [Key]
+        public int StudentID { get; set; }  
+        public DateTime SelectionDate { get; set; }  
+
+     
+        public virtual Student? Student { get; set; }
+    }
+}
